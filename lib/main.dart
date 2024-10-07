418dbb4ec7376e711d7405d4d33a7696643649b9
SamCT <colliertenisonsa@hendrix.edu>
2024-10-07 16:51:11 -0500

    router added, main updated, flutter configured

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
 // Copyright 2022 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';               
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';    
 
+import 'app_state.dart';
 import 'home_page.dart';
 
 void main() {
-  runApp(const App());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(ChangeNotifierProvider(
+    create: (context) => ApplicationState(),
+    builder: ((context, child) => const App()),
+  ));
 }
 
 class App extends StatelessWidget {
